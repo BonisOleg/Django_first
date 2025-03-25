293bb8385d635c3a36b1615944e638fba4835c71
BonisOleg <olegbonislav@gmail.com>
2025-03-25 23:11:19 +0200

    Проєкт з каталогом і першими сторінками

---
+++ products/models.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
